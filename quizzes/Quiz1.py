4bfaeaa9b8c6b11dc8e14c4a4158ffbc2efcfb7c
Glenn Downing <downing@cs.utexas.edu>
2013-03-11 20:47:35 -0700

    another commit

--- quizzes/Quiz1.py
+++ quizzes/Quiz1.py
@@ -6,6 +6,23 @@ OOPL JPL: Quiz #1
 
 """ ----------------------------------------------------------------------
 1. What does the following program do?
+
+1. <type 'bool'>
+2. <type 'type'>
+3. <type 'exceptions.NameError'>
+4. <type 'type'>
+5. <type 'type'>
+g1 f1 f2 g2 g4 g5 g6
+g1 f1 g3 g5 g6
+g1 f1 g5
+Traceback (most recent call last):
+  File "./Quiz1.py", line 40, in <module>
+    g(2)
+  File "./Quiz1.py", line 28, in g
+    f(b)
+  File "./Quiz1.py", line 22, in f
+    raise TypeError()
+TypeError
 """
 
 print "1.", type(False)
