6e1e771d089f5f0e62f35d57456d9e496a1d097c
Glenn Downing <downing@cs.utexas.edu>
2013-03-12 17:01:25 -0700

    another commit

--- quizzes/Quiz2.py
+++ quizzes/Quiz2.py
@@ -6,6 +6,9 @@ OOPL JPL: Quiz #2
 
 """ ----------------------------------------------------------------------
 1. What is the output of the following program?
+
+5
+11
 """
 
 def f (n) :
@@ -16,9 +19,26 @@ print f(7)
 
 """ ----------------------------------------------------------------------
 2. In the context of Project #1: Collatz, what is f() computing?
+
+For odd n it's computing (3n + 1) / 2.
+(3n + 1) / 2
+3n/2 + 1/2
+n + n/2 + 1/2
+n + n/2 + 1, because n is odd
+n + (n >> 1) + 1
 """
 
 """ ----------------------------------------------------------------------
 3. Given positive integers, b and e, let m = e / 2. If b < m, then
    max_cycle_length(b, e) = max_cycle_length(m, e). True or False?
+
+True
+
+Consider b = 10, e = 100.
+Then m = 100 / 2 = 50.
+max_cycle_length(10, 100) = max_cycle_length(50, 100)
+All the numbers in the range [10, 49] can be mapped to numbers in the
+range [50, 100] by one or more doublings, so none of the numbers in the
+range [10, 49] could have a larger cycle length than the numbers in the
+range [50, 100].
 """
