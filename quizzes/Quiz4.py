00fc007e2b712f2cea05cb217752df2d5fb5d6ec
Glenn Downing <downing@cs.utexas.edu>
2013-03-14 10:59:52 -0700

    another commit

--- quizzes/Quiz4.py
+++ quizzes/Quiz4.py
@@ -13,6 +13,33 @@ OOPL JPL: Quiz #4
 
 import operator
 
+def reduce_1 (bf, a, *z) :
+    if (not a) and (not z) :
+        raise TypeError("reduce() of empty sequence with no initial value")
+    p = iter(a)
+    if not z :
+        v = p.next()
+    else :
+        v = z[0]
+    try :
+        while True :
+            v = bf(v, p.next())
+    except StopIteration :
+        pass
+    return v
+
+def reduce_2 (bf, a, *z) :
+    if (not a) and (not z) :
+        raise TypeError("reduce() of empty sequence with no initial value")
+    if not z :
+        a = iter(a)
+        v = a.next()
+    else :
+        v = z[0]
+    for w in a :
+        v = bf(v, w)
+    return v
+
 def test_reduce (f) :
     try :
         assert f(operator.add, []) ==  0
@@ -33,5 +60,6 @@ def test_reduce (f) :
     assert f(operator.sub, [2, 3, 4], 0) == -9 # 0 - 2 - 3 - 4
     assert f(operator.mul, [2, 3, 4], 1) == 24 # 1 * 2 * 3 * 4
 
-test_reduce(my_reduce)
-test_reduce(   reduce)
+test_reduce(reduce_1)
+test_reduce(reduce_2)
+test_reduce(reduce)
