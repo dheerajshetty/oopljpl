014d9554212ab4dbe3b0ee59eab98749e8e35f26
Glenn Downing <downing@cs.utexas.edu>
2013-03-15 10:49:15 -0700

    another commit

--- quizzes/Quiz5.py
+++ quizzes/Quiz5.py
@@ -10,38 +10,22 @@ OOPL JPL: Quiz #5
 
 from Queue import PriorityQueue
 
-class MyPriorityQueue (object) :
-    def __init__ (self) :
-        self.a = []
-
-    def empty (self) :
-        return self.qsize() == 0
-
-    def get (self) :
-        v = min(self.a)
-        self.a.remove(v)
-        return v
-
-    def put (self, v) :
-        self.a.append(v)
-
-    def qsize (self) :
-        return len(self.a)
 
 def test_priority_queue (c) :
     x = c()
     assert x.qsize() == 0
     assert x.empty()
 
-    x.put(3)
-    x.put(4)
     x.put(2)
-    assert x.qsize() == 3
+    x.put(1)
+    x.put(4)
+    x.put(3)
+    assert x.qsize() == 4
     assert not x.empty()
 
     v = x.get()
-    assert v         == 2
-    assert x.qsize() == 2
+    assert v         == 1
+    assert x.qsize() == 3
     assert not x.empty()
 
 test_priority_queue(MyPriorityQueue)
